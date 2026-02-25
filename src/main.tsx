b4f4a7d0309dbdddf161d74afc2e6bf6da8dcff9
bracko87 <glupostimejlza@gmail.com>
2026-02-25 23:16:12 +0100

    Remove unused Leaflet CSS import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import './shadcn.css'
-import 'leaflet/dist/leaflet.css'
 import App from './App'
 
 const root = createRoot(document.getElementById('app')!)
